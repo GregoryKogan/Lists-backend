381504b3e1c6a816bba0b3ea066c0e51b3adc52a
GregoryKogan <grisha.koganovskiy@gmail.com>
2022-11-25 02:27:22 +0300

    Notes list endpoint returns only user's notes

--- Backend/lists/notes/views.py
+++ Backend/lists/notes/views.py
@@ -1,16 +1,38 @@
-from notes.models import Note
+from notes.models import Note, Item
 from notes.serializers import NoteSerializer
 from rest_framework import generics, permissions
 from notes.permissions import IsOwnerOrReadOnly
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class NoteList(generics.ListCreateAPIView):
-    queryset = Note.objects.all()
-    serializer_class = NoteSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+# class NoteList(generics.ListCreateAPIView):
+#     queryset = Note.objects.all()
+#     serializer_class = NoteSerializer
+#     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+
+class NoteList(APIView):
+    """
+    List all notes, or create a new snippet.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        notes = Note.objects.filter(owner=request.user)
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
