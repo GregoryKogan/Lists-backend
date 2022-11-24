dfb37911aeeb21e5a9f9e1cf2e532316b6845435
GregoryKogan <grisha.koganovskiy@gmail.com>
2022-11-25 02:30:43 +0300

    Note endpoints permissions changed

--- Backend/lists/notes/views.py
+++ Backend/lists/notes/views.py
@@ -1,20 +1,12 @@
 from notes.models import Note, Item
 from notes.serializers import NoteSerializer
 from rest_framework import generics, permissions
-from notes.permissions import IsOwnerOrReadOnly
+from notes.permissions import IsOwner
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-
-# class NoteList(generics.ListCreateAPIView):
-#     queryset = Note.objects.all()
-#     serializer_class = NoteSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-
-#     def perform_create(self, serializer):
-#         serializer.save(owner=self.request.user)
 
 class NoteList(APIView):
     """
@@ -38,4 +30,4 @@ class NoteList(APIView):
 class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
