f2b46945d166813dd3d086c567a89f1d232188bd
GregoryKogan <grisha.koganovskiy@gmail.com>
2022-11-25 03:12:29 +0300

    Items Of Note endpoit created

--- Backend/lists/notes/urls.py
+++ Backend/lists/notes/urls.py
@@ -5,6 +5,7 @@ from notes import views
 urlpatterns = [
     path("notes/", views.NoteList.as_view()),
     path("notes/<int:pk>/", views.NoteDetail.as_view()),
+    path("items/<int:note_pk>/", views.ItemsOfNote.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- Backend/lists/notes/views.py
+++ Backend/lists/notes/views.py
@@ -1,8 +1,9 @@
 from notes.models import Note, Item
-from notes.serializers import NoteSerializer
+from notes.serializers import NoteSerializer, ItemSerializer
 from rest_framework import generics, permissions
 from notes.permissions import IsOwner
 from django.http import Http404
+from django.core.exceptions import PermissionDenied
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,7 +11,7 @@ from rest_framework import status
 
 class NoteList(APIView):
     """
-    List all notes, or create a new snippet.
+    List all notes, or create a new note.
     """
 
     permission_classes = [permissions.IsAuthenticated]
@@ -32,3 +33,34 @@ class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+
+class ItemsOfNote(APIView):
+    """
+    List all items of some note or create a new item in it.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_note(self, request, note_pk):
+        try:
+            note = Note.objects.get(pk=note_pk)
+            if note.owner == request.user:
+                return note
+            else:
+                raise PermissionDenied
+        except Note.DoesNotExist as e:
+            raise Http404 from e
+
+    def get(self, request, note_pk, format=None):
+        note = self.get_note(request, note_pk)
+        items = Item.objects.filter(note=note)
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, note_pk, format=None):
+        note = self.get_note(request, note_pk)
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=self.request.user, note=note)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
